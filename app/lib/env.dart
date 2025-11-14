ea590226cd52e0afdee2a10d0dc805a84f90ed3f
Lexa2805 <165327278+Lexa2805@users.noreply.github.com>
2025-11-14 16:46:14 +0200

    Changes database

--- app/lib/env.dart
+++ app/lib/env.dart
@@ -4,3 +4,5 @@ class Env {
   static const supabaseUrl = 'https://injupfwmmptlitxozulp.supabase.co';
   static const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImluanVwZndtbXB0bGl0eG96dWxwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMwNjcyMzAsImV4cCI6MjA3ODY0MzIzMH0.BobhqF-hteXuNthz0eiOav1XXm7m48cXuPOPcMuQtow';
 }
+
+
